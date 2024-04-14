7e4134ab5db090620474c332e8e15617309e60a4
Honjes <honjes@protonmail.com>
2024-04-14 08:11:42 +0200

    fix: change EventSchema to array

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -12,40 +12,42 @@ const IDSchema = z.object({
   id: IDType,
 })
 
-const EventSchema = z.object({
-  type: z.string().openapi({
-    example: 'Event',
-  }),
-  id: IDType,
-  link: z.string().openapi({
-    example:
-      'https://www.vlr.gg/event/1927/champions-tour-2023-china-ascension',
-  }),
-  name: z.string().openapi({
-    example: 'Champions Tour 2023 China: Ascension',
-  }),
-  date: z.string().openapi({
-    example: 'Dec 22—30',
-  }),
-  status: z
-    .string()
-    .regex(/New||Ongoing||Completed/)
-    .openapi({
-      example: 'completed',
+const EventSchema = z
+  .object({
+    type: z.string().openapi({
+      example: 'Event',
     }),
-  prize: z.string().openapi({
-    example: '$250,000',
-  }),
-  region: z
-    .string()
-    .regex(/EU||NA||KR||BR||AP||LATAM||OCE/)
-    .openapi({
-      example: 'EU',
+    id: IDType,
+    link: z.string().openapi({
+      example:
+        'https://www.vlr.gg/event/1927/champions-tour-2023-china-ascension',
     }),
-  logo: z.string().openapi({
-    example: 'https://owcdn.net/img/6009f963577f4.png',
-  }),
-})
+    name: z.string().openapi({
+      example: 'Champions Tour 2023 China: Ascension',
+    }),
+    date: z.string().openapi({
+      example: 'Dec 22—30',
+    }),
+    status: z
+      .string()
+      .regex(/New||Ongoing||Completed/)
+      .openapi({
+        example: 'completed',
+      }),
+    prize: z.string().openapi({
+      example: '$250,000',
+    }),
+    region: z
+      .string()
+      .regex(/EU||NA||KR||BR||AP||LATAM||OCE/)
+      .openapi({
+        example: 'EU',
+      }),
+    logo: z.string().openapi({
+      example: 'https://owcdn.net/img/6009f963577f4.png',
+    }),
+  })
+  .array()
 
 const TeamSchema = z.object({
   name: z.string().openapi({
@@ -70,11 +72,11 @@ const GameSchema = z.object({
 })
 const StreamSchema = z.object({
   name: z.string(),
-  link: z.string()
+  link: z.string(),
 })
 const PlayerSchema = z.object({
   name: z.string(),
-  link: z.string()
+  link: z.string(),
 })
 const MatchSchema = z.object({
   type: z.string().openapi({
