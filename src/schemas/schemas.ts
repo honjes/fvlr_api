dd09eaecc80c7455da632c2cea1a5aa50551c743
Honjes <honjes@protonmail.com>
2024-04-14 09:34:47 +0200

    feat: add zod Objects for stats

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -12,7 +12,172 @@ export const regionsEnum = z.enum([
 ])
 export const typeEnum = z.enum(['Event', 'Match'])
 
-// Types
+// Util Objects
+// Object for the stats of a player
+const statsObject = z
+  .object({
+    kdr: z.string(),
+    acs: z.string(),
+    k: z.string(),
+    d: z.string(),
+    a: z.string(),
+    kdb: z.string(),
+    kast: z.string(),
+    adr: z.string(),
+    hs: z.string(),
+    fk: z.string(),
+    fd: z.string(),
+    fkdb: z.string(),
+  })
+  .openapi({
+    example: {
+      kdr: '1.20',
+      acs: '230',
+      k: '29',
+      d: '21',
+      a: '6',
+      kdb: '+8',
+      kast: '69%',
+      adr: '162',
+      hs: '37%',
+      fk: '2',
+      fd: '2',
+      fkdb: '0',
+    },
+  })
+export type Stats = z.infer<typeof statsObject>
+
+// Object for the stats of a player per site
+const extStatsObject = z
+  .object({
+    kdr: z.object({
+      ct: z.string(),
+      t: z.string(),
+      ot: z.string(),
+    }),
+    acs: z.object({
+      ct: z.string(),
+      t: z.string(),
+      ot: z.string(),
+    }),
+    k: z.object({
+      ct: z.string(),
+      t: z.string(),
+      ot: z.string(),
+    }),
+    d: z.object({
+      ct: z.string(),
+      t: z.string(),
+      ot: z.string(),
+    }),
+    a: z.object({
+      ct: z.string(),
+      t: z.string(),
+      ot: z.string(),
+    }),
+    kdb: z.object({
+      ct: z.string(),
+      t: z.string(),
+      ot: z.string(),
+    }),
+    kast: z.object({
+      ct: z.string(),
+      t: z.string(),
+      ot: z.string(),
+    }),
+    adr: z.object({
+      ct: z.string(),
+      t: z.string(),
+      ot: z.string(),
+    }),
+    hs: z.object({
+      ct: z.string(),
+      t: z.string(),
+      ot: z.string(),
+    }),
+    fk: z.object({
+      ct: z.string(),
+      t: z.string(),
+      ot: z.string(),
+    }),
+    fd: z.object({
+      ct: z.string(),
+      t: z.string(),
+      ot: z.string(),
+    }),
+    fkdb: z.object({
+      ct: z.string(),
+      t: z.string(),
+      ot: z.string(),
+    }),
+  })
+  .openapi({
+    example: {
+      kdr: {
+        ct: '0.88',
+        t: '1.51',
+        ot: '',
+      },
+      acs: {
+        ct: '127',
+        t: '334',
+        ot: '',
+      },
+      k: {
+        ct: '7',
+        t: '22',
+        ot: '',
+      },
+      d: {
+        ct: '11',
+        t: '10',
+        ot: '',
+      },
+      a: {
+        ct: '2',
+        t: '4',
+        ot: '',
+      },
+      kdb: {
+        ct: '-4',
+        t: '+12',
+        ot: '',
+      },
+      kast: {
+        ct: '56%',
+        t: '83%',
+        ot: '',
+      },
+      adr: {
+        ct: '108',
+        t: '216',
+        ot: '',
+      },
+      hs: {
+        ct: '40%',
+        t: '35%',
+        ot: '',
+      },
+      fk: {
+        ct: '0',
+        t: '2',
+        ot: '',
+      },
+      fd: {
+        ct: '1',
+        t: '1',
+        ot: '',
+      },
+      fkdb: {
+        ct: '-1',
+        t: '+1',
+        ot: '',
+      },
+    },
+  })
+export type ExtStats = z.infer<typeof extStatsObject>
+
+// Schemas
 const IDType = z
   .string()
   .min(1)
