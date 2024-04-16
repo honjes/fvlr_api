dd3bfafbd6688484f3bb1c05578ed09b8997b26c
Honjes <honjes@protonmail.com>
2024-04-16 05:36:58 +0200

    feat: use types in scraper

--- src/scrapers/team/one.ts
+++ src/scrapers/team/one.ts
@@ -4,11 +4,9 @@
 import { load } from 'cheerio'
 import { idGenerator } from '../util'
 import { PORT } from '../..'
+import { Player, Team } from '../../schemas/schemas'
 
-const fetchOneTeam = async (id: string): Promise<Object> => {
-  // Validate input
-  // make sure id is a string of numbers
-  if (!id.match(/^[0-9,]+$/)) throw new Error('Invalid ID')
+const fetchOneTeam = async (id: string): Promise<Team> => {
   return new Promise(async (resolve, reject) => {
     // fetch the page
     fetch(`https://www.vlr.gg/team/${id}`)
@@ -23,7 +21,7 @@ const fetchOneTeam = async (id: string): Promise<Object> => {
             .includes('Page not found')
         )
           reject('404')
-        let Team = new Object()
+        let Team = new Object() as Team
         // Team Name,, Logo, Region, Socials, Roster, Staff, Earnings
         Team.name = $('h1.wf-title').text().trim()
         Team.tag = $('h2.wf-title.team-header-tag').text().trim()
@@ -39,7 +37,7 @@ const fetchOneTeam = async (id: string): Promise<Object> => {
         if (Team.logo.includes('/img/vlr/tmp/vlr.png'))
           Team.logo = 'https://www.vlr.gg/img/vlr/tmp/vlr.png'
         // Get Socials
-        $('.team-header-links a').each(async (i, element) => {
+        $('.team-header-links a').each((i, element) => {
           let socialLink = $(element).attr('href')
           if (socialLink === '' || socialLink === undefined) return
           // add https if not there
@@ -50,7 +48,7 @@ const fetchOneTeam = async (id: string): Promise<Object> => {
             socialURL = new URL(socialLink)
           } catch {
             console.error(
-              'Error setting socialURL: ' + socialLink + ' | Page: ' + url,
+              'Error setting socialURL: ' + socialLink + ' | Page: ',
               false
             )
             console.info('FUCKING SOCIAL LINKS CAN SUCK MY ASS')
@@ -61,7 +59,7 @@ const fetchOneTeam = async (id: string): Promise<Object> => {
           Team.socials.push({ name: socialName, link: socialLink })
         })
         // Generaate Players and Staff
-        Team.players_item = new Array()
+        const players_item: any[] = new Array()
         Team.staff_item = new Array()
         $('.team-roster-item').each((i, element) => {
           const playerLink = $(element).find('.team-roster-item a').attr('href')
@@ -82,7 +80,7 @@ const fetchOneTeam = async (id: string): Promise<Object> => {
             .trim()
           if (playerRole === '' || playerRole === undefined) {
             playerRole = 'player'
-            Team.players_item.push({
+            players_item.push({
               ign: playerIgn,
               link: `https://www.vlr.gg${playerLink}`,
               id: idGenerator(playerId),
@@ -98,24 +96,19 @@ const fetchOneTeam = async (id: string): Promise<Object> => {
           }
         })
 
-        // Generate team.players array of ids
-        Team.players = new Array()
-        Team.players_item.forEach((player) => {
-          Team.players.push(idGenerator(player.id))
-        })
         // SICK way of keeping players cached ;)
-        const PlayerPromises = [
-          ...Team.players_item.map((player) => {
+        const playerPromises = [
+          ...players_item.map((player) => {
             return fetch(`http://localhost:${PORT}/player/${player.id}`)
           }),
           ...Team.staff_item.map((player) => {
             return fetch(`http://localhost:${PORT}/player/${player.id}`)
           }),
         ]
-        let Players = await Promise.all(PlayerPromises)
-        Players = await Promise.all(Players.map((res) => res.json()))
-        Team.players = Players.map((player) => player.data)
-        delete Team.players_item
+        let players = await Promise.all(playerPromises)
+        players = await Promise.all(players.map((res) => res.json()))
+        Team.players = players.map((player: any) => player.data)
+
         // Generate team.staff array of ids
         Team.staff = new Array()
         Team.staff_item.forEach((player) => {
