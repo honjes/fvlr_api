840db878fc7c2c309646074c2c02249b51463102
Honjes <honjes@protonmail.com>
2024-04-16 07:42:00 +0200

    feat: event scraper now uses types

--- src/scrapers/events/one.ts
+++ src/scrapers/events/one.ts
@@ -4,15 +4,9 @@
 import { load } from 'cheerio'
 import { idGenerator } from '../util'
 // Schema
-import { z } from '@hono/zod-openapi'
-import { EventSchema } from '../../schemas/schemas'
-// Type
-type Event = z.infer<typeof EventSchema>
+import { Event, typeEnum } from '../../schemas/schemas'
 
-const fetchOneEvent = async (id: string): Promise<Object> => {
-  // Validate input
-  // make sure id is a string of numbers
-  if (!id.match(/^[0-9]+$/)) throw new Error('Invalid ID')
+const fetchOneEvent = async (id: string): Promise<Event> => {
   return new Promise(async (resolve, reject) => {
     // fetch the page
     fetch(`https://www.vlr.gg/event/${id}`)
@@ -27,22 +21,22 @@ const fetchOneEvent = async (id: string): Promise<Object> => {
         )
           reject('404')
         const Event = {} as Event
-        Event.type = 'event'
+        Event.type = typeEnum.Enum.Event
         Event.name = $('h1.wf-title').text().trim()
         Event.link = `https://www.vlr.gg/event/${id}`
         Event.id = id
-        Event.img =
+        Event.logo =
           'https:' + $('.wf-avatar.event-header-thumb img').attr('src')
         // Get all teams
         const Teams = new Array()
         $('.event-team').each((i, element) => {
-          const team = new Object()
+          const team = new Object() as Event['teams_item'][0]
           try {
             team.name = $(element).find('.event-team-name').text().trim()
-            team.logo = $(element)
-              .find('.event-team-players-mask > img')
-              .attr('src')
-            team.link = $(element).find('.event-team-name').attr('href')
+            team.logo =
+              $(element).find('.event-team-players-mask > img').attr('src') ||
+              ''
+            team.link = $(element).find('.event-team-name').attr('href') || ''
             team.id = idGenerator(team.link.split('/')[2])
             team.players = new Array()
             team.status = 'ok'
@@ -57,7 +51,7 @@ const fetchOneEvent = async (id: string): Promise<Object> => {
         const Players = new Array()
         const FailedLinks = new Array()
         $('.event-team-players-item').each((i, element) => {
-          const playerLink = $(element).attr('href')
+          const playerLink = $(element).attr('href') || ''
           let playerId
           try {
             playerId = idGenerator(playerLink.split('/')[2])
@@ -74,12 +68,13 @@ const fetchOneEvent = async (id: string): Promise<Object> => {
             .text()
             .trim()
           const playerTeamId = idGenerator(
-            $(element)
-              .parent()
-              .parent()
-              .find('.event-team-name')
-              .attr('href')
-              .split('/')[2]
+            (
+              $(element)
+                .parent()
+                .parent()
+                .find('.event-team-name')
+                .attr('href') || ''
+            ).split('/')[2]
           )
           for (let i = 0; i < Teams.length; i++) {
             if (Teams[i].name == playerTeamName) {
