1dcd153079cdad9c5897372596e1116a09a5b850
Honjes <honjes@protonmail.com>
2024-04-16 09:19:16 +0200

    feat: score scraper uses types
    - score scraper uses types
    - score now uses route instead of directly calling the function

--- src/scrapers/matches/score.ts
+++ src/scrapers/matches/score.ts
@@ -1,40 +1,34 @@
-import { fetchOneMatch } from "./one"
 // Schema
 import { z } from '@hono/zod-openapi'
-import { MatchSchema } from '../../schemas/schemas'
+import { MatchSchema, Score } from '../../schemas/schemas'
+import { PORT } from '../..'
 // Types
 type Match = z.infer<typeof MatchSchema>
 
-type ScoresObject = {
-    [index: string]:Number
-}
-type PlayerScoreObject = {
-    name: String,
-    score: Number
-}
-
-const generateScore = async (id: string) => {
-    const match : Match = await fetchOneMatch(id);
-    const scores_object: ScoresObject = {};
-    const scores_array: PlayerScoreObject[] = [];
-    // Itterate through the players, generate score based on formula
-    match.players.forEach((player)=>{
-        let score = 0;
-        score += Number(player.stats.acs);
-        score += Number(player.stats.k) * 4;
-        score -= Number(player.stats.d) * 3;
-        score += Number(player.stats.a) * 2;
-        score += Number(player.stats.fk) * 5;
-        score -= Number(player.stats.fd) * 4;
-        scores_object[player.name] = score;
-        scores_array.push({"name": player.name, "score": score})
-    })
-    return {
-        id,
-        scores_array,
-        scores_object,
-        match
-    };
+const generateScore = async (id: string): Promise<Score> => {
+  const match: Match = await (
+    await fetch(`localhost:${PORT}/match/${id}`).then((res) => res.json())
+  ).data
+  const scores_object: Score['scores_object'] = {}
+  const scores_array: Score['scores_array'] = []
+  // Itterate through the players, generate score based on formula
+  match.players.forEach((player) => {
+    let score = 0
+    score += Number(player.stats.acs)
+    score += Number(player.stats.k) * 4
+    score -= Number(player.stats.d) * 3
+    score += Number(player.stats.a) * 2
+    score += Number(player.stats.fk) * 5
+    score -= Number(player.stats.fd) * 4
+    scores_object[player.name] = score
+    scores_array.push({ name: player.name, score: score })
+  })
+  return {
+    id,
+    scores_array,
+    scores_object,
+    match,
+  }
 }
 
-export {generateScore}
+export { generateScore }
