7abb86d2eb1f027e5ce06443f1aac9babdeb38cc
Honjes <honjes@protonmail.com>
2024-04-16 05:36:33 +0200

    feat: add schema for /teams/id endpoint

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -363,6 +363,29 @@ export const playerSchema = z.object({
 })
 export type Player = z.infer<typeof playerSchema>
 
+// Schema for the /teams/{id} endpoint
+export const teamSchema = z.object({
+  id: IDType,
+  name: z.string(),
+  tag: z.string(),
+  logo: z.string(),
+  country: z.string(),
+  link: z.string(),
+  socials: z.array(z.object({ name: z.string(), link: z.string() })),
+  earnings: z.string(),
+  players: z.array(playerSchema),
+  staff: z.array(z.string()),
+  staff_item: z.array(
+    z.object({
+      ign: z.string(),
+      link: z.string(),
+      id: IDType,
+      role: z.string(),
+    })
+  ),
+})
+export type Team = z.infer<typeof teamSchema>
+
 // Schema for the /matches/{id} endpoint
 export const MatchSchema = z.object({
   type: typeEnum.openapi({
