6f1835ed4f08d0452d59025a7a0cffdeab16047b
Clara <honjes@protonmail.com>
2025-01-03 16:47:18 +0100

    code: update Event Response

--- src/scrapers/events/all.ts
+++ src/scrapers/events/all.ts
@@ -18,46 +18,49 @@ const fetchAllEvents = (page: number = 1): Promise<ShortEvent> => {
       .then((data) => {
         const $ = load(data)
         $('.event-item').each((i, element) => {
-          const Event = {} as ShortEventElement
-          Event.type = typeEnum.Enum.Event
-          Event.link = `https://www.vlr.gg` + $(element).attr('href')
-          Event.id = idGenerator(Event.link.split('/')[4])
-          Event.name = $(element).find('.event-item-title').text().trim()
-          Event.date = $(element)
-            .find('.event-item-desc-item.mod-dates')
-            .text()
-            .trim()
-            .split('\t')[0]
-          Event.status = $(element)
-            .find('.event-item-desc-item-status')
-            .text()
-            .trim()
-          Event.prize = $(element)
-            .find('.event-item-desc-item.mod-prize')
-            .text()
-            .trim()
-            .split('\t')[0]
-          Event.region = regionsEnum.parse(
-            $(element)
-              .find('.event-item-desc-item.mod-location > i')
-              .attr('class')
-              ?.split(' ')[1]
-              .split('-')[1]
-              .toUpperCase()
-          )
-          Event.logo =
-            'https:' + $(element).find('.event-item-thumb > img').attr('src')
-          if (!Event.logo.includes('https://')) {
+          const link = `https://www.vlr.gg` + $(element).attr('href')
+          const eventItem: ShortEventElement = {
+            type: typeEnum.Enum.Event,
+            id: idGenerator(link.split('/')[4]),
+            link: link,
+            name: $(element).find('.event-item-title').text().trim(),
+            date: $(element)
+              .find('.event-item-desc-item.mod-dates')
+              .text()
+              .trim()
+              .split('\t')[0],
+            status: $(element)
+              .find('.event-item-desc-item-status')
+              .text()
+              .trim(),
+            prize: $(element)
+              .find('.event-item-desc-item.mod-prize')
+              .text()
+              .trim()
+              .split('\t')[0],
+            region: regionsEnum.parse(
+              $(element)
+                .find('.event-item-desc-item.mod-location > i')
+                .attr('class')
+                ?.split(' ')[1]
+                .split('-')[1]
+                .toUpperCase()
+            ),
+            logo:
+              'https:' + $(element).find('.event-item-thumb > img').attr('src'),
+          }
+          // Check if logo is https
+          if (!eventItem.logo.includes('https://')) {
             // Check if it has 1 slash or none
-            if (Event.logo.includes('https:/')) {
+            if (eventItem.logo.includes('https:/')) {
               // It has 1 slash, add another
-              Event.logo = Event.logo.replace('https:/', 'https://')
+              eventItem.logo = eventItem.logo.replace('https:/', 'https://')
             } else {
               // It has no slashes, add 2
-              Event.logo = Event.logo.replace('https:', 'https://')
+              eventItem.logo = eventItem.logo.replace('https:', 'https://')
             }
           }
-          Events.push(Event)
+          Events.push(eventItem)
         })
         resolve(Events)
       })
